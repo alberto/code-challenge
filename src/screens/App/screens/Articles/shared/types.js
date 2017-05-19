4d7ddaa8c739a3a42ab4cb41ff71cd296bd9384f
alberto <alberto.rdrz@gmail.com>
2017-05-19 13:05:39 +0200

    Add missing type properties

--- src/screens/App/screens/Articles/shared/types.js
+++ src/screens/App/screens/Articles/shared/types.js
@@ -2,4 +2,6 @@ import { PropTypes } from 'prop-types';
 
 export const ArticleListItemType = PropTypes.shape({
   title: PropTypes.string,
+  author: PropTypes.string,
+  excerpt: PropTypes.string,
 });
