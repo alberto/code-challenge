5a5d74a1e3237434dc53e0c3bac7184a8d8bc64d
alberto <alberto.rdrz@gmail.com>
2017-05-21 14:17:06 +0200

    Fix network warning in App test

--- src/screens/App/App.test.js
+++ src/screens/App/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import nock from 'nock';
+import axios from 'axios';
+import httpAdapter from 'axios/lib/adapters/http';
+
 import App from './';
+
+axios.defaults.adapter = httpAdapter;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  nock('http://localhost:4000')
+    .post('/graphql')
+    .reply(200, { data: { articles: [] } });
   ReactDOM.render(<App />, div);
 });
