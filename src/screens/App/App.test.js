ef0c7146c2d73bd8d54f5b78100c24355c3bfaa3
alberto <alberto.rdrz@gmail.com>
2017-05-21 23:17:19 +0200

    Made with ❤

--- src/screens/App/App.test.js
+++ src/screens/App/App.test.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom';
 import nock from 'nock';
 import axios from 'axios';
 import httpAdapter from 'axios/lib/adapters/http';
+import { mount } from 'enzyme';
 
 import App from './';
 
 axios.defaults.adapter = httpAdapter;
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  nock('http://localhost:4000')
-    .post('/graphql')
-    .reply(200, { data: { articles: [] } });
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    nock('http://localhost:4000')
+      .post('/graphql')
+      .reply(200, { data: { articles: [] } });
+    ReactDOM.render(<App />, div);
+  });
+
+  it('was made with ❤', () => {
+    nock('http://localhost:4000')
+      .post('/graphql')
+      .reply(200, { data: { articles: [] } });
+    const wrapper = mount(<App />);
+    expect(wrapper.text()).toMatch('♥');
+  });
 });
