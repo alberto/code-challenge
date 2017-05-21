8e11ba9c7ebbb33e579ef14d3e66d744824cb477
alberto <alberto.rdrz@gmail.com>
2017-05-21 16:40:51 +0200

    Extract fragments in queries

--- src/screens/App/screens/Articles/shared/queries.js
+++ src/screens/App/screens/Articles/shared/queries.js
@@ -1,21 +1,37 @@
-export const ARTICLES_QUERY = `{
-  articles {
-    author
-    excerpt
-    id
-    title
-  }
-}`;
+const ARTICLE_SUMMARY_FRAGMENT = `
+fragment ArticleSummary on Article {
+  author
+  excerpt
+  id
+  title
+}
+`;
 
-export const ARTICLE_QUERY = `
-query articleById($id: String!) {
-  article(id: $id) {
+const ARTICLE_DETAIL_FRAGMENT = `
+fragment ArticleDetail on Article {
     author
     content
     id
     published
     tags
     title
+}
+`;
+export const ARTICLES_QUERY = `
+${ARTICLE_SUMMARY_FRAGMENT}
+
+query {
+  articles {
+    ...ArticleSummary
+  }
+}`;
+
+export const ARTICLE_QUERY = `
+${ARTICLE_DETAIL_FRAGMENT}
+
+query articleById($id: String!) {
+  article(id: $id) {
+    ...ArticleDetail
   }
 }
 `;
@@ -29,26 +45,19 @@ mutation articleDelete($id: String!) {
 `;
 
 export const ARTICLE_CREATE_MUTATION = `
+${ARTICLE_DETAIL_FRAGMENT}
+
 mutation articleCreate($author: String, $content: String, $excerpt: String, $published: Boolean, $tags: [String], $title: String) {
   articleCreate(author: $author, content: $content, excerpt: $excerpt, published: $published, tags: $tags, title: $title) {
-    author
-    content
-    excerpt
-    published
-    tags
-    title
+    ...ArticleDetail
   }
 }`;
 
 export const ARTICLE_UPDATE_MUTATION = `
+${ARTICLE_DETAIL_FRAGMENT}
+
 mutation articleUpdate($id: String!, $author: String, $content: String, $excerpt: String, $published: Boolean, $tags: [String], $title: String) {
   articleUpdate(id: $id, author: $author, content: $content, excerpt: $excerpt, published: $published, tags: $tags, title: $title) {
-    id,
-    author
-    content
-    excerpt
-    published
-    tags
-    title
+    ...ArticleDetail
   }
 }`;
